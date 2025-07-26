385c118c885b376c230866bd1788572758ac5e38
Petar Petrovic <petarbijanacfon@yahoo.com>
2025-07-26 20:58:35 +0200

    Dodate i rute za autentifikaciju i testiranje preko postmana

--- sluzba-backend/routes/api.php
+++ sluzba-backend/routes/api.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserAuthController;
 
-Route::get('/test', function () {
-    return response()->json(['message' => 'Radi API ruta!']);
+Route::post('/register', [UserAuthController::class, 'register']);
+Route::post('/login', [UserAuthController::class, 'login']);
+
+// Grupisana ruta za autentifikovane korisnike
+Route::middleware('auth:sanctum')->group(function () {
+    
+   
+    Route::post('/logout', [UserAuthController::class, 'logout']);
+
+    
 });
