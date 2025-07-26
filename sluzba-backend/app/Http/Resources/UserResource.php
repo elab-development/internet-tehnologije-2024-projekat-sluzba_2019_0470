7877537b1601bbab98d665b01420d4bad8d588a9
Petar Petrovic <petarbijanacfon@yahoo.com>
2025-07-26 20:56:20 +0200

    Autentifikacija korisnika

---
+++ sluzba-backend/app/Http/Resources/UserResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+         return [
+            'ime' => $this->ime,
+            'prezime' => $this->prezime,
+            'broj_indeksa' => $this->broj_indeksa,
+            'godina_studija' => $this->godina_studija,
+            'smer' => $this->smer,
+        ];
+    }
+}
